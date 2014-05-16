9d378ca4fabe41a1551f6747514f5cb72e39b290
Justin Nelson <justin.john.nelson@gmail.com>
2014-05-15 21:26:26 -0500

    Added sln

---
+++ DataAccessExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan time;
+            var nums = Time(() => AllNumbers(), out time);
+            Console.WriteLine("Total seconds: " + time.TotalSeconds);
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadKey();
+        }
+
+        public static IEnumerable<string> AllNumbers()
+        {
+            using (var con = GetConnection())
+            {
+                var sqlCommand = new MySqlCommand("SELECT * FROM positivenumbers", con);
+                var rdr = sqlCommand.ExecuteReader();
+                while (rdr.Read())
+                {
+                    yield return rdr["number"].ToString();
+                }
+            }
+        }
+
+        private static MySqlConnection GetConnection()
+        {
+            string connStr = "server=localhost;user=jjnguy;database=generatorexample;port=3307;password=asdf1234;";
+            var con = new MySqlConnection(connStr);
+            con.Open();
+            return con;
+        }
+
+        private static T Time<T>(Func<T> code, out TimeSpan timeResult)
+        {
+            var start = DateTime.UtcNow;
+            var codeResult = code();
+            var end = DateTime.UtcNow;
+            timeResult = end.Subtract(start);
+            return codeResult;
+        }
+    }
+}
---
+++ IntroToGenerators/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToGenerators
+{
+    public class Program
+    {
+        static void Main(string[] Args)
+        {
+            try
+            {
+                var allNumbers = Count();
+                foreach (var num in allNumbers)
+                {
+                    Console.WriteLine(num);
+                }
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Lists every positive int
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<int> Count()
+        {
+            // what goes here?
+            for (var i = 0; i < int.MaxValue; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
